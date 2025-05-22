24194c3ce8f83b9aea632d3c8cb0d81cfeffe56e
reshmaanilkumar13 <reshmaanilkumar04@gmail.com>
2025-05-22 09:20:02 +0530

    Add files via upload

--- tkinter-1.py
+++ tkinter-1.py
@@ -1,9 +1,9 @@
 #importing 
-from tkinter import *
-import tkinter as tk
+#from tkinter import *
+#import tkinter as tk
 root = tk.Tk() 
 root.title("My GUI")
-#root.geometry("250x250")
-#root.maxsize(300,300)
-#root.minsize(200,200)
-#root.mainloop()
+root.geometry("250x250")
+root.maxsize(300,300)
+root.minsize(200,200)
+root.mainloop()
